028adee29ab25d17269d071453187eba0d217e85
Raniro Coelho <ranirocoelho@gmail.com>
2024-07-23 22:52:27 -0300

    Update tailwind custom example names to generic ones

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,15 +7,15 @@ export default {
     theme: {
       extend: {
         colors: {
-          'tractian': {
+          'custom': {
             'blue': {
                 100: '#55A6FF',
                 200: '#2188FF',
                 300: '#023B78',
                 400: '#17192D',
             },
-            'green':'#52C41A',
-            'red':'#ED3833',
+            'custom-green':'#52C41A',
+            'custom-red':'#ED3833',
           },
       },
     },
